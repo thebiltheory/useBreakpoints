c54e8ec2a33ec257bcc0cc337d6f8084c84476b8
Bil Benhamou <bil.benhamou@gmail.com>
2020-04-29 01:51:54 +0200

    fix: add a browser checkpoint

--- src/utils/makeMediaQueryList.ts
+++ src/utils/makeMediaQueryList.ts
@@ -1,6 +1,11 @@
+import { isBrowser } from './isBrowser'
+
 export default function makeMediaQueryList(breakpoints: number[]) {
   return breakpoints.map((breakpoint: number, index: number) => {
     const maxWidth = breakpoints[index + 1]
+
+    if (!isBrowser) return
+
     return window.matchMedia(
       `(min-width: ${breakpoint}px) ${
         maxWidth ? `and (max-width: ${maxWidth - 1}px)` : ''
